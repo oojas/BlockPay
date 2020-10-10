37706e5c9bf13dadf9eac9a2d13a91e9edfb4b31
oojas <oojasgupta@gmail.com>
2020-10-11 02:05:55 +0530

    Initializing commit

---
+++ lib/First.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
+
+class web extends StatefulWidget {
+  @override
+  _webState createState() => _webState();
+}
+
+class _webState extends State<web> {
+  Client httpcl;
+  Web3Client ethcl;
+  final address = "0x197b4a6ECf9aE7Bb5593417A10ee645b2d9EFAa0";
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'First.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: web(),
+  ));
+}
