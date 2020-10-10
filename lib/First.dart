958009b64f9aca40466e5d882c51fa0daf1bc2b6
oojas <oojasgupta@gmail.com>
2020-10-11 02:55:45 +0530

    Added Buttons

--- lib/First.dart
+++ lib/First.dart
@@ -11,6 +11,7 @@ class web extends StatefulWidget {
 class _webState extends State<web> {
   Client httpcl;
   Web3Client ethcl;
+  bool data = false;
   final address = "0x197b4a6ECf9aE7Bb5593417A10ee645b2d9EFAa0";
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,44 @@ class _webState extends State<web> {
             .make(),
         VStack([
           (context.percentHeight * 10).heightBox,
-          "\$BlockPay".text.xl4.white.bold.center.makeCentered().py16()
+          "\$BlockPay".text.xl4.white.bold.center.makeCentered().py16(),
+          (context.percentHeight * 5).heightBox,
+          VxBox(
+                  child: VStack([
+            "Balance".text.gray700.xl2.semiBold.makeCentered(),
+            10.heightBox,
+            data
+                ? "\$1".text.bold.xl6.makeCentered()
+                : CircularProgressIndicator().centered()
+          ]))
+              .p16
+              .white
+              .size(context.screenWidth, context.percentHeight * 18)
+              .rounded
+              .shadowXl
+              .make()
+              .p16(),
+          30.heightBox,
+          HStack([
+            FlatButton.icon(
+                onPressed: () {},
+                color: Colors.blue,
+                shape: Vx.roundedSm,
+                icon: Icon(Icons.refresh),
+                label: "Refresh".text.white.bold.make()),
+            FlatButton.icon(
+                onPressed: () {},
+                color: Colors.blue,
+                shape: Vx.roundedSm,
+                icon: Icon(Icons.refresh),
+                label: "Refresh".text.white.bold.make()),
+            FlatButton.icon(
+                onPressed: () {},
+                color: Colors.blue,
+                shape: Vx.roundedSm,
+                icon: Icon(Icons.refresh),
+                label: "Refresh".text.white.bold.make())
+          ])
         ])
       ]),
     );
